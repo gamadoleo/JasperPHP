7180201ef7d124bba8872f964a5eb6a4d6adef61
gamadoleo <gamadoleo@hotmail.com>
2021-06-13 12:03:03 -0300

    Create Frame.php
    This file adds the basic functionality of the jasper Frame object, for example the studio templates use this element in the report footer, and without it the footer doesn't work.
    It now remains to improve its functionality, for example testing in detail the Frame is not respecting the auto height strech

---
+++ src/Frame.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace JasperPHP;
+
+use \JasperPHP;
+
+/**
+ * classe TLabel
+ * classe para construção do elemento Frame
+ *
+ * @author   Leandro Gama <gamadoleo@hotmail.com.br>
+ * @version  2021.06.13
+ * @access   restrict
+ * 
+ * 2021.06.13 -- criação
+ * */
+class Frame extends Element {
+
+    public function generate($obj = null) {
+        $row = is_array($obj) ? $obj[1] : array();
+        $obj = is_array($obj) ? $obj[0] : $obj;
+        if ($this->children) {
+            foreach ($this->children as $child) {
+                // se for objeto
+                if (is_object($child)) {
+                    $child->generate(array($obj, $row));
+                }
+            }
+        }
+    }
+
+}
+
+?>
